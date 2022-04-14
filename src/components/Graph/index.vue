838c8c3b191cbf090193388d80c3580d7587dbd1
caigx <caigx@zxjmail.com>
2022-04-14 16:47:08 +0800

    feat：增加基础交互

--- src/components/Graph/index.vue
+++ src/components/Graph/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="graph-item absolute" :id="data.id" :style="parseStyle(data)">
+  <div class="graph-item absolute" :id="data.id" :style="parseStyle(data)" @mouseover="onHover">
     <component :is="data.type + '-graph'" />
   </div>
 </template>
 
 <script lang="ts">
   import { BaseGraph } from '@/utils/Graph';
+  import { AnchorStaticId, EndpointOptions } from 'jsplumb';
   import { Component, Prop, Vue } from 'vue-property-decorator';
   import RectangleGraph from './Rectangle.vue';
   @Component({
@@ -33,9 +34,69 @@
       };
       return style;
     }
+
+    /**
+     * 
+     */
+    onHover(evt: MouseEvent) {
+      console.log('evt:', evt)
+    }
+
+    /**
+     * 设置基础配置
+     */
+    setBase() {
+      const elId = this.data.id;
+      this.$jpIns.draggable(elId);
+
+      // const exampleGreyEndpointOptions: EndpointOptions = {
+      //   endpoint: 'Rectangle',
+      //   isSource: true,
+      //   maxConnections: -1,
+      //   connectorStyle: { stroke: '#666' },
+      //   isTarget: true,
+      // };
+
+      const anchors: AnchorStaticId[] = ['Top', 'Bottom', 'Left', 'Right'];
+      const endPoints: EndpointOptions[] = [];
+
+      anchors.forEach(val => {
+        endPoints.push({
+          anchor: val,
+          endpoint: ['Dot', {
+            radius: 5
+          }],
+          isSource: true,
+          maxConnections: -1,
+          connectorStyle: { 
+            stroke: '#e6e6e6', 
+            strokeWidth: 2
+          },
+          paintStyle: {
+            fill: 'transparent'
+          },
+          hoverPaintStyle: {
+            fill: 'red'
+          },
+          isTarget: true,
+        })
+      })
+      
+      this.$jpIns.addEndpoints(elId, endPoints);
+    }
+
+    mounted() {
+      this.setBase();
+    }
   }
 </script>
 
-<style scoped>
+<style lang="less">
+  .end-point {
+    background-color: #EEE;
 
+    &-hover {
+      background-color: #DDD;
+    }
+  }
 </style>
--- src/components/GraphArea/index.vue
+++ src/components/GraphArea/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full relative">
+  <div id="graph-area" class="w-full h-full relative">
     <graph-item v-for="item in list" :key="item.id" :data="item" />
   </div>
 </template>
@@ -7,7 +7,7 @@
 <script lang="ts">
   import { BaseGraph } from '@/utils/Graph';
   import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
-  import { GraphAreaChangeEvent, StartPoint } from './index';
+  import { GraphAreaChangeEvent } from './index';
   import GraphItem from '@/components/Graph/index.vue';
 
   @Component({
@@ -26,85 +26,61 @@
     // Props end
 
     // data start
-    /**
-     * 是否在图形上按下鼠标
-     */
-    isDown: boolean = false;
-    /**
-     * 记录按下鼠标的位置
-     */
-    startPoint: StartPoint = {
-      x: 0,
-      y: 0,
-      oldLeft: 0,
-      oldTop: 0
-    }
-    /**
-     * 记录按下的图形
-     */
-    downItem: BaseGraph | null = null;
+    
     // data end
 
     // methods start
-    /**
-     * 鼠标在图形上按下
-     */
-    onMousedown(evt: MouseEvent, item: BaseGraph) {
-      if (this.isDown) {
-        return ;
-      }
-      this.downItem = item;
-      this.isDown = true;
-      Object.assign(this.startPoint, {
-        x: evt.pageX,
-        y: evt.pageY,
-        oldLeft: item.left,
-        oldTop: item.top
-      })
-    }  
-
-    /**
-     * 监听鼠标移动
-     */
-    onMousemove(evt: MouseEvent) {
-      if (!this.isDown) return ;
-      const downItem = this.downItem;
-      if (downItem == null) return ;
-      evt.stopPropagation();
-      const startPoint = this.startPoint;
-      const { pageX, pageY } = evt;
-      let left = pageX - startPoint.x + startPoint.oldLeft;
-      let top = pageY - startPoint.y + startPoint.oldTop;
-      
-      downItem.left = left;
-      downItem.top = top;
-    }
-
-    /**
-     * 监听松开鼠标
-     */
-    onMouseup() {
-      if (this.isDown) {
-        this.downItem = null;
-        this.isDown = false;
-      }
-    }
 
     @Emit()
     change(data: GraphAreaChangeEvent) {
       return data;
     }
+
+    /**
+     * 设置jsPlumb基础配置
+     */
+    setJPBase() {
+      const $jpIns = this.$jpIns;
+      // 设置 jsPlumb 容器
+      $jpIns.setContainer('#graph-area')
+      // 建立连接
+      $jpIns.bind('connection', info => {
+        // (info.connection as any).bind('click', (info: any) => {
+        //   console.log('connection click：', info)
+        // })
+        console.log('connection info：', info)
+      })
+      // 点击连接线
+      $jpIns.bind('click', connection => {
+        // connection.set
+        console.log('click', connection)
+      })
+      // 双击连接线
+      $jpIns.bind('dblclick', con => {
+        console.log('dblclick：', con)
+      })
+      // 点击终点
+      $jpIns.bind('endpointDblClick', endpoint => {
+        console.log('endpointDblClick info：', endpoint)
+      })
+    }
+    /**
+     * 清除jsPlumb基础配置
+     */
+    clearJPBase() {
+      const $jpIns = this.$jpIns;
+      $jpIns.unbind('click');
+      $jpIns.unbind('connection');
+    }
     // methods end
 
     // 挂载完毕
     mounted() {
-      document.addEventListener('mousemove', this.onMousemove)
-      document.addEventListener('mouseup', this.onMouseup)
+      this.setJPBase();
     }
     // 销毁前
     beforeDestroy() {
-      document.removeEventListener('mousemove', this.onMousemove)
-      document.removeEventListener('mouseup', this.onMouseup)
+      this.clearJPBase();
     }
   }
 </script>
